b30b00d6ad5a95dff4bc41b04e95bbc3b1a05ed0
Malika Liyanage <alrider12@gmail.com>
2021-05-17 03:22:28 +0800

    Fixed indentation errors detected by linter

--- tests/FormValidatorTest.php
+++ tests/FormValidatorTest.php
@@ -4,68 +4,68 @@ use PHPUnit\Framework\TestCase;
 
 final class FormValidatorTest extends TestCase
 {
-  public function testCanBeInitialized()
-  {
-    $this->assertInstanceOf(
-      FormValidator::class,
-      new FormValidator()
-    );
-  }
+    public function testCanBeInitialized()
+    {
+        $this->assertInstanceOf(
+            FormValidator::class,
+            new FormValidator()
+        );
+    }
 
-  public function testSanitizeString()
-  {
-    $formValidator = new FormValidator();
+    public function testSanitizeString()
+    {
+        $formValidator = new FormValidator();
 
-    $this->assertEquals(
-      'Mason Jason',
-      $formValidator->sanitizeString('Mason Jason')
-    );
+        $this->assertEquals(
+            'Mason Jason',
+            $formValidator->sanitizeString('Mason Jason')
+        );
 
-    $this->assertEquals(
-      'Mason Jason',
-      $formValidator->sanitizeString('<h1>Mason Jason</h1>')
-    );
-  }
+        $this->assertEquals(
+            'Mason Jason',
+            $formValidator->sanitizeString('<h1>Mason Jason</h1>')
+        );
+    }
 
-  public function testSanitizeEmail()
-  {
-    $formValidator = new FormValidator();
+    public function testSanitizeEmail()
+    {
+        $formValidator = new FormValidator();
 
-    $this->assertEquals(
-      'test@test.com',
-      $formValidator->sanitizeEmail('test@test.com')
-    );
+        $this->assertEquals(
+            'test@test.com',
+            $formValidator->sanitizeEmail('test@test.com')
+        );
 
-    $this->assertEquals(
-      'test@test.com',
-      $formValidator->sanitizeEmail('<test@test.com;')
-    );
-  }
+        $this->assertEquals(
+            'test@test.com',
+            $formValidator->sanitizeEmail('<test@test.com;')
+        );
+    }
 
-  public function testIsValidEmail()
-  {
-    $formValidator = new FormValidator();
+    public function testIsValidEmail()
+    {
+        $formValidator = new FormValidator();
 
-    $this->assertTrue(
-      $formValidator->isValidEmail('test@test.com')
-    );
+        $this->assertTrue(
+            $formValidator->isValidEmail('test@test.com')
+        );
 
-    $this->assertFalse(
-      $formValidator->isValidEmail('<test@test.com;')
-    );
-  }
+        $this->assertFalse(
+            $formValidator->isValidEmail('<test@test.com;')
+        );
+    }
 
-  public function testTrimString()
-  {
-    $formValidator = new FormValidator();
+    public function testTrimString()
+    {
+        $formValidator = new FormValidator();
 
-    $this->assertEmpty(
-      $formValidator->trimString('            ')
-    );
+        $this->assertEmpty(
+            $formValidator->trimString('            ')
+        );
 
-    $this->assertEquals(
-      'test@test.com',
-      $formValidator->trimString(' test@test.com ')
-    );
-  }
+        $this->assertEquals(
+            'test@test.com',
+            $formValidator->trimString(' test@test.com ')
+        );
+    }
 }
