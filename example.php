1f2cd3671abd9149451818279b368c33bf327df8
Dancer-14863 <g14863.malika@gmail.com>
2021-05-16 21:57:59 +0800

    updated style links

--- example.php
+++ example.php
@@ -6,7 +6,8 @@
     <meta name="description" content="Homepage">
     <title>Homepage</title>
 
-    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" type="text/css" href="css/nav.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
     <!-- Bootstrap Plugin-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
