2439e76cc91c01bd388d44f0d327fd44b117e25b
Malika Liyanage <alrider12@gmail.com>
2021-05-17 04:15:28 +0800

    Added form validator class

---
+++ php/FormValidator.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+class FormValidator
+{
+  public function __construct()
+  {
+  }
+
+  public function sanitizeString(string $string): string
+  {
+    return filter_var($this->trimString($string), FILTER_SANITIZE_STRING);
+  }
+
+  public function sanitizeEmail(string $email): string
+  {
+    return filter_var($this->trimString($email), FILTER_SANITIZE_EMAIL);
+  }
+
+  public function isValidEmail(string $email): bool
+  {
+    return filter_var($this->trimString($email), FILTER_VALIDATE_EMAIL);
+  }
+
+  public function trimString(string $string): string
+  {
+    return trim($string);
+  }
+
+}
